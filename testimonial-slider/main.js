f5fd8b699e15270d7d0b2b99e7ca23597d97dba8
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2025-03-10 20:22:20 +0530

    restaurant menu  router, store

--- testimonial-slider/main.js
+++ testimonial-slider/main.js
@@ -1,5 +1,11 @@
-const data = [
-  {
-    img:""
+let $testimonialBoxes = document.querySelectorAll('.testimonial-box');
+
+function showBox() {
+  for (let box of $testimonialBoxes) {
+    setInterval(() => {
+      box.classList.add('active');
+    }, 1000);
   }
-];
+}
+
+showBox();
